4179ca938b103e8c14f6a39c1011ad0252577fbf
Kiran Joshi <kiranjoshi893@gmail.com>
2023-02-13 17:20:49 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import * as React from 'react';
 import './style.css';
 
 export default function App() {
+  console.log(process.env, 'REACT_APP_PROJECT_ENV')
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :) tttt</p>
+      <h1>Hello there!</h1>
     </div>
   );
 }
