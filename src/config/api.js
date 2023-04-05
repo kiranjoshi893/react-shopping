485bfb03331e552fcdd91b4d6c0df704bd91f36b
Kiran Joshi <kiranjoshi893@gmail.com>
2023-04-05 19:21:45 +0530

    fixes

--- src/config/api.js
+++
@@ -1,8 +0,0 @@
-import settings from "./settings";
-
-let url = settings.api.url
-export default {
-    LOGIN: url + 'auth/login',
-    ALLPRODUCTS: url + "products",
-    ALLCATEGORY: url + "categories"
-}
