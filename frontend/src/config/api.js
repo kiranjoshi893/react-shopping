222704ff2c85db5c9f5251c64e554d0efa43b098
Kiran Joshi <45868481+kiranjoshi893@users.noreply.github.com>
2023-04-05 21:00:08 +0530

    Delete frontend directory

--- frontend/src/config/api.js
+++
@@ -1,9 +0,0 @@
-import settings from "./settings";
-
-let url = settings.api.url
-export default {
-    LOGIN: url + 'login',
-    SIGNUP: url + 'signup',
-    ALLPRODUCTS: url + "products",
-    ALLCATEGORY: url + "categories"
-}
