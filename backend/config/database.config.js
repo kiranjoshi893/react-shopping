d71953b390605199e5e6ee41944c89b076435904
Kiran Joshi <45868481+kiranjoshi893@users.noreply.github.com>
2023-04-05 20:59:04 +0530

    Delete backend directory

--- backend/config/database.config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    url:'mongodb+srv://admin:admin321@learning.eauwn.mongodb.net/ecommerce-api?retryWrites=true&w=majority'
-}
--- backend/config/database.configexample.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    url:'mongodb+srv://<username>:<password>@learning.eauwn.mongodb.net/<database>?retryWrites=true&w=majority'
-}
