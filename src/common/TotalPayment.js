f08da90f1af2b5ac03f4612350e9d618a82215d3
Kiran Joshi <kiranjoshi893@gmail.com>
2023-05-08 12:15:50 +0000

    price calcutaion

--- src/common/TotalPayment.js
+++ src/common/TotalPayment.js
@@ -2,14 +2,17 @@ import React from 'react'
 
 const TotalPayment = ({data}) => {
     console.log(data, 'props:121211312')
+    const onlyPrice  = data.map(value => parseInt(value.original_price) * value.qty)
+    // const convertIntoNumber = onlyPrice.map(Number)
+    const sum  = onlyPrice.reduce((a, b) => a + b)
+    console.log(onlyPrice, sum, 'onlyPriceonlyPrice')
     return(
         <>
             <h5 className='fw-bold mb-3 border-bottom pb-3'>Price Details</h5>
             <div className='d-flex justify-content-between mb-2 pb-1'>
                 <h6>Price ({data.length} items)</h6>
-                <h6>$ 6556</h6>
+                <h6>$ {sum}</h6>
             </div>
-            {data.map((item) => <div>{item.qty * item.original_price}</div>)}
             <div className='d-flex justify-content-between mb-2 pb-1'>
                 <h6>Delivery Charges</h6>
                 <h6>Free</h6>
@@ -17,10 +20,10 @@ const TotalPayment = ({data}) => {
 
             <div className='d-flex border-top border-bottom mb-3 pt-3 justify-content-between mb-2 pb-1'>
                 <h5 className='fw-bold'>Total Amount</h5>
-                <h5 className='fw-bold'>$ 3,610</h5>
+                <h5 className='fw-bold'>$ {sum}</h5>
             </div>
 
-            <h6 className='text-success'>You will save ₹2,860 on this order</h6>
+            {/* <h6 className='text-success'>You will save ₹2,860 on this order</h6> */}
         </>
     )
 }
